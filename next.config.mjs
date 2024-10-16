3f93ffd3b6ec7aabd55b659a6ba5fb28ad5fda17
Auspicious <info@auspicioussoft.com>
2024-10-16 15:58:15 +0530

    Refactor image URL generation and add AWS S3 support

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost'],
-      },
+  images: {
+    domains: [
+      'localhost',
+      `${process.env.AWS_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com`,
+    ],
+  },
 };
 
 export default nextConfig;
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,11 @@
 'use server'
 
 import { signIn, signOut } from "@/auth"
+import { s3Client } from "@/config/s3"
+// import { s3Client } from "@/config/s3"
 import { loginService, signupService } from "@/services/user-service"
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { cookies } from "next/headers"
 
 export const loginAction = async (payload: any) => {
@@ -14,7 +18,7 @@ export const loginAction = async (payload: any) => {
                 name: res.data.data.firstName + ' ' + res.data.data.lastName,
                 _id: res.data.data._id,
                 myReferralCode: res.data.data.myReferralCode,
-                profilePic: res.data.data.profilePic,   
+                profilePic: res.data.data.profilePic,
                 redirect: false,
             },
             )
@@ -46,4 +50,33 @@ export const logoutAction = async () => {
 export const getTokenCustom = async () => {
     const cookiesOfNextAuth = cookies().get("authjs.session-token")
     return cookiesOfNextAuth?.value!
+}
+
+// Upload to S3
+export const uploadFile = async (file: any, userId: string) => {
+    const uploadParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `projects/${userId}/${file.name}`,
+        Body: file,
+        ContentType: file.type,
+    }
+    try {
+        const command = new PutObjectCommand(uploadParams);
+        await s3Client.send(command);
+        console.log("File uploaded successfully.");
+    } catch (error) {
+        console.error("Error uploading file:", error);
+    }
+}
+
+//Get file from S3
+export const getImageUrl = async (userId: string, imageName: string) => {
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `projects/${userId}/${imageName}`,
+    };
+
+    const command = new GetObjectCommand(params);
+    const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 }) // URL valid for 1 hour
+    return url
 }
