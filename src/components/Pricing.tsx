253a70ed2f16009afdcdf89460309d44c37393bf
Auspicious <info@auspicioussoft.com>
2024-11-04 10:53:51 +0530

    Refactor Pricing component to incorporate billing interval and update plan selection logic

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -11,12 +11,12 @@ const PricingPlans: React.FC = () => {
   const session = useSession();
   const { data, isLoading } = useSWR(`/user/${session.data?.user?.id}`, getUserInfo, { revalidateOnFocus: false });
   const currentPlan = data?.data.data?.planType;
-  
-  const [billingPeriod, setBillingPeriod] = useState<'month' | 'year'>('month');
+  const interval = data?.data.data?.planInterval;
+  const [billingPeriod, setBillingPeriod] = useState<'month' | 'year'>(interval === 'month' ? 'month' : 'year');
 
   const handlePlanSelect = async (planType: string) => {
     try {
-      const response = await buyPlan(`/user/${session.data?.user?.id}/buy-plan`, { planType })
+      const response = await buyPlan(`/user/${session.data?.user?.id}/buy-plan`, { planType, interval: billingPeriod });
       const data = await response.data;
       if (data.id) {
         const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
@@ -35,16 +35,16 @@ const PricingPlans: React.FC = () => {
       <div className="my-[40px] md:mt-[55px] md:mb-[100px] text-center">
         <h2 className="section-title mb-5">Choose Your <span>Pricing Plan</span></h2>
         <div className='mb-5 md:mb-[45px]'>
-          <select 
-            value={billingPeriod} 
-            onChange={(e) => setBillingPeriod(e.target.value as 'month' | 'year')} 
-            className='w-full max-w-[150px] bg-[#fff]  font-semibold text-[#262626] py-2 px-5 text-lg rounded-[10px] '
+          <select
+            value={billingPeriod}
+            onChange={(e) => setBillingPeriod(e.target.value as 'month' | 'year')}
+            className='w-full max-w-[150px] bg-[#fff]  font-semibold text-[#262626] py-2 px-5 text-lg rounded-[10px] cursor-pointer'
           >
-            <option value="month">Month</option>
-            <option value="year">Year</option>
+            <option value="month" className='!cursor-pointer'>Month</option>
+            <option value="year" className='!cursor-pointer'>Year</option>
           </select>
         </div>
-        
+
         <div className="grid md:grid-cols-3 gap-5 lg:gap-[57px] ">
           {/* Free Trial Plan */}
           <div className="border border-[#262626] rounded-[10px] py-5 px-4 bg-white md:mt-10">
@@ -72,7 +72,7 @@ const PricingPlans: React.FC = () => {
           <div className="rounded-[10px] px-[17px] pt-5 md:pt-[13px] pb-7 bg-[#E56F20] text-white">
             <p className="text-[24px] font-semibold mb-[30px] border border-white rounded-[10px] inline-block px-5 py-1">Intro Plan</p>
             <h2 className="md:text-[40px] text-[30px] font-[600] leading-[normal] ">
-            {billingPeriod === 'month' ? '€12.50' : (
+              {billingPeriod === 'month' ? '€12.50' : (
                 <>
                   <span className="line-through text-[30px] mr-2">€150</span> €142.50
                 </>
@@ -90,8 +90,8 @@ const PricingPlans: React.FC = () => {
               <li>Workspace Users (1)</li>
               <li>Create your avatar with just a picture (Unlimited)</li>
             </ul>
-            <button disabled={currentPlan === 'intro'} onClick={() => handlePlanSelect('intro')} className="px-4 lg:px-[50px] mt-7 py-3 text-base lg:text-[20px] bg-white text-[#E56F20] rounded-[10px] font-semibold font-inter">
-              {currentPlan === 'intro' ? 'Current Plan' : 'Select Plan'}
+            <button disabled={currentPlan === 'intro' && interval === billingPeriod} onClick={() => handlePlanSelect('intro')} className="px-4 lg:px-[50px] mt-7 py-3 text-base lg:text-[20px] bg-white text-[#E56F20] rounded-[10px] font-semibold font-inter">
+              {currentPlan === 'intro' ? interval === billingPeriod ? 'Current Plan' : 'Select Plan' : 'Select Plan'}
             </button>
           </div>
 
@@ -102,10 +102,10 @@ const PricingPlans: React.FC = () => {
                 <p className="text-[24px] font-semibold mb-3 border border-[#262626] text-[#262626] rounded-[10px] inline-block px-5 py-1">Pro Plan</p>
                 <h2 className="md:text-[40px] text-[30px] font-[600] leading-[normal] text-[#262626]">
                   {billingPeriod === 'month' ? '€37.50' : (
-                <>
-                  <span className="line-through text-[30px] mr-2">€450</span> €427.5
-                </>
-              )}
+                    <>
+                      <span className="line-through text-[30px] mr-2">€450</span> €427.5
+                    </>
+                  )}
                 </h2>
                 <p className="text-sm text-[#81A7BA] mb-5">User/{billingPeriod === 'month' ? 'Month' : 'Year'}</p>
                 <ul className="text-left half-list orange-check list-design">
@@ -122,8 +122,8 @@ const PricingPlans: React.FC = () => {
                 </ul>
               </div>
               <div>
-                <button disabled={currentPlan === 'pro'} onClick={() => handlePlanSelect('pro')} className="px-4 lg:px-[50px] mt-7 py-3 text-base lg:text-[20px] text-white bg-[#E56F20] rounded-[10px] font-semibold font-inter">
-                  {currentPlan === 'pro' ? 'Current Plan' : 'Select Plan'}
+                <button disabled={currentPlan === 'pro' && interval === billingPeriod} onClick={() => handlePlanSelect('pro')} className="px-4 lg:px-[50px] mt-7 py-3 text-base lg:text-[20px] text-white bg-[#E56F20] rounded-[10px] font-semibold font-inter">
+                  {currentPlan === 'pro' ? interval === billingPeriod ? 'Current Plan' : 'Current Plan (Yearly)' : 'Select Plan'}
                 </button>
               </div>
             </div>
