1f1a3357d553d31e5206503f5b751be55df530cb
humanshi123 <humanshi.auspicioussoft@gmail.com.com>
2024-11-25 17:10:04 +0530

    privacy-added

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { Suspense } from 'react'
+"use client";
+import { Suspense, useState } from "react";
 import Image from "next/image";
 import logo from "@/assets/images/logo.png";
 import LoginCard from "@/components/LoginCard";
@@ -9,44 +9,64 @@ import { useEffect } from "react";
 import { toast } from "sonner";
 import { signupAction } from "@/actions";
 import { useSession } from "next-auth/react";
-
+import Modal from "react-modal";
+import PrivacyPolicy from "@/components/PrivacyPolicy";
+import { CrossIcon } from "@/utils/svgIcons";
 export default function Signup() {
+  const [opemModal, setOpenModal] = useState(false);
   const SearchParamsFunction = () => {
     const { data: session } = useSession();
     const router = useRouter();
-    const searchParams = useSearchParams()
-    const referralCode = searchParams.get('referralCode') ?? '';
+    const searchParams = useSearchParams();
+    const referralCode = searchParams.get("referralCode") ?? "";
     useEffect(() => {
       if (session) {
         router.push("/");
       }
     }, [session, router]);
 
-    const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (
+      event
+    ) => {
       event.preventDefault();
       const formData = new FormData(event.currentTarget);
-      const email = formData.get('email') as string;
-      const password = formData.get('password') as string;
-      const firstName = formData.get('firstName') as string;
-      const lastName = formData.get('lastName') as string;
+      const email = formData.get("email") as string;
+      const password = formData.get("password") as string;
+      const firstName = formData.get("firstName") as string;
+      const lastName = formData.get("lastName") as string;
 
-      if (!email || !password || !firstName || !lastName) return toast.error('All fields are required');
-      const resss = await signupAction({ email, password, firstName, lastName, ...(referralCode && { referralCode }) });
+      if (!email || !password || !firstName || !lastName)
+        return toast.error("All fields are required");
+      const resss = await signupAction({
+        email,
+        password,
+        firstName,
+        lastName,
+        ...(referralCode && { referralCode }),
+      });
       if (resss?.success) {
-        toast.success('Signed up successfully');
-        router.push('/login');
+        toast.success("Signed up successfully");
+        router.push("/login");
       } else {
-        toast.error(Array.isArray(resss?.message) ? resss?.message[0].message : resss?.message);
+        toast.error(
+          Array.isArray(resss?.message)
+            ? resss?.message[0].message
+            : resss?.message
+        );
       }
-    }
+    };
     return (
       <div className="">
         <div className="grid md:grid-cols-2 gap-y-10 items-center">
           <div className="bg-[#F5F7FA] flex flex-col justify-center mobile-space lg:pl-[113px] md:pr-4 h-full">
             <div className="md:max-w-[418px] 2xl:mx-auto">
               <Image src={logo} height={100} width={200} alt="Logo" />
-              <h1 className="main-title mt-[30px] md:mt-[94px] mb-[5px] md:mb-3">Sign Up</h1>
-              <p className="login-desc mb-5 md:mb-10">Create a new account and see the magic of AI.</p>
+              <h1 className="main-title mt-[30px] md:mt-[94px] mb-[5px] md:mb-3">
+                Sign Up
+              </h1>
+              <p className="login-desc mb-5 md:mb-10">
+                Create a new account and see the magic of AI.
+              </p>
               <form onSubmit={handleSubmit}>
                 <div className="mb-2 md:mb-[15px]">
                   <input
@@ -89,6 +109,12 @@ export default function Signup() {
                   />
                 </div>
                 <div>
+                  <label htmlFor="" className="flex items-center gap-2 mb-5 text-[#686c78] ">
+                    <input type="checkbox" name="" className="w-auto" />I agree to the
+                    <span onClick={()=> setOpenModal(true)} className="cursor-pointer text-[#e87223] font-bold  ">Privacy Policy</span>
+                  </label>
+                </div>
+                <div>
                   <button
                     type="submit"
                     className="button inline-block text-center md:leading-7 w-full bg-[#e87223] rounded-[5px] text-white text-base p-[15px]"
@@ -97,7 +123,9 @@ export default function Signup() {
                   </button>
                 </div>
               </form>
-              <p className="login-desc mt-[20px] md:mt-[153px]">Copyright © 2020 - 2025 NeurCG.</p>
+              <p className="login-desc mt-[20px] md:mt-[153px]">
+                Copyright © 2020 - 2025 NeurCG.
+              </p>
             </div>
           </div>
           <div className="waves">
@@ -106,14 +134,25 @@ export default function Signup() {
             </div>
           </div>
         </div>
+        <Modal isOpen={opemModal} onRequestClose={() => setOpenModal(false)}
+         className="modal w-full md:max-w-[70%] h-[90vh] p-4 md:p-[30px] pt-[50px]  rounded-[20px] overflo-custom overflow-y-auto relative bg-white "
+         overlayClassName="z-[10] px-2 md:p-0 w-full h-full fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+         bodyOpenClassName='overflow-hidden'
+         >
+          <button
+            className="absolute top-4 right-5"
+            onClick={() => setOpenModal(false)}
+          > <CrossIcon />
+          </button>
+          <PrivacyPolicy />
+        </Modal>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <SearchParamsFunction />
     </Suspense>
-  )
-
+  );
 }
---
+++ src/components/PrivacyPolicy.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className="privacy-policy">
+            <h1>Privacy Policy</h1>
+            <p><strong>Last updated:</strong> 01.07.2024</p>
+
+            <section>
+                <h2>Introduction</h2>
+                <p>Welcome to NeurCG GmbH!</p>
+                <p>
+                    We are committed to protecting your personal information and your right to privacy. This Privacy Policy explains what information we collect, how we use it, and what rights you have in relation to it. If you have any questions or concerns, please contact us at <a href="mailto:neurcg@gmail.com">neurcg@gmail.com</a>.
+                </p>
+            </section>
+
+            <section>
+                <h2>1. Data Controller</h2>
+                <address>
+                <p>The data controller responsible for your personal data is:
+                </p>
+                    <p><strong>NeurCG GmbH</strong></p>
+                    <p>Fuerstrasse 19</p>
+                    <p>72072 Tübingen</p>
+                    <p>Email: <a href="mailto:neurcg@gmail.com">neurcg@gmail.com</a></p>
+                    <p>Phone: +49 1512 8950793</p>
+                </address>
+            </section>
+
+            <section>
+                <h2>2. Information We Collect</h2>
+                <p>
+                We collect personal information that you voluntarily provide to us when you register on the website, express interest in obtaining information about us or our products and services, participate in activities on the website, or otherwise contact us.
+
+The personal information that we collect depends on the context of your interactions with us and the website, the choices you make, and the products and features you use. The personal information we collect may include the following:
+                </p>
+        
+                <ul>
+                    <li><strong>Name and Contact Data:</strong> We collect your first and last name, email address, postal address, phone number, and other similar contact data.</li>
+                    <li><strong>Credentials:</strong>We collect passwords, password hints, and similar security information used for authentication and account access.</li>
+                    <li><strong>Payment Data:</strong>We collect data necessary to process your payment if you make purchases, such as your payment instrument number (e.g., credit card number), and the security code associated with your payment instrument.</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>3. How We Use Your Information</h2>
+                <p>We use personal information collected via our website for a variety of business purposes described below:</p>
+                <ul>
+                    <li>To facilitate account creation and logon process.</li>
+                    <li>To send administrative information to you for our business purposes, such as information regarding our services and changes to our terms, conditions, and policies.</li>
+                    <li>To fulfill and manage your orders, payments, returns, and exchanges made through the website.</li>
+                    <li>To send you marketing and promotional communications.</li>
+                    <li>To enforce our terms, conditions, and policies for business purposes, to comply with legal and regulatory requirements, or in connection with our contract.</li>
+                    <li>To respond to legal requests and prevent harm.</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>4. Sharing Your Information</h2>
+                <p>We only share information with your consent, to comply with laws, to provide you with services, to protect your rights, or to fulfill business obligations. We may process or share data based on the following legal basis:</p>
+                <ul>
+                    <li><strong>Consent:</strong>We may process your data if you have given us specific consent to use your personal information for a specific purpose.</li>
+                    <li><strong>Legitimate Interests:</strong> We may process your data when it is reasonably necessary to achieve our legitimate business interests.</li>
+                    <li><strong>Performance of a Contract:</strong> Where we have entered into a contract with you, we may process your personal information to fulfill the terms of our contract.</li>
+                    <li><strong>Legal Obligations:</strong> We may disclose your information where we are legally required to do so in order to comply with applicable law, governmental requests, a judicial proceeding, court order, or legal process.</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>5. Data Security</h2>
+                <p>
+                We have implemented appropriate technical and organizational security measures designed to protect the security of any personal information we process. However, please also remember that we cannot guarantee that the internet itself is 100% secure.
+                </p>
+            </section>
+
+            <section>
+                <h2>6. Your Data Protection Rights</h2>
+                <p>If your personal data are processed, then you are a data subject within the language of GDPR, and you have the following rights vis-à-vis the controller:</p>
+                <ul>
+                    <li><strong>Right to Information:</strong> <br />
+                    According to Art. 15 GDPR, you have a right to information. Specifically, this means you have the right to request confirmation from the data controller as to whether we process personal data pertaining to you. If this is the case, you also have the right to obtain information about these personal data, and about the information set forth in Art. 15 (1) GDPR. This includes, for instance, information about the purposes for processing, about the categories of personal data processed, and about the recipients or categories of recipients to whom the personal data have been or will be disclosed (Art. 15 (1) lit. a), b) and c) GDPR).</li>
+                    <li><strong>Right to Rectification:</strong><br />
+                    According to Art. 16 GDPR, you have a right to rectification. What this means specifically is that you have the right to request immediate rectification of incorrect personal data about you as well as completion of incomplete personal data.</li>
+                    <li><strong>Right to Erasure:</strong><br />
+                    Art. 17 GDPR grants you a right to erasure of data about you (also known as the “right to be forgotten”). In other words, you have the right to request that we erase personal data relating to you immediately, and we have an obligation to erase personal data at once if one of the reasons set forth in Art. 17 (1) GDPR applies. This may be the case, for example, if personal data are no longer needed to fulfil the purposes for which they were collected or were otherwise processed (Art. 17 (1) lit. a) GDPR).</li>
+                     <li>If we have made the personal data public and are obliged to erase them, we are also under an obligation, taking account of available technology and the cost of implementation, to take reasonable steps, including technical ones, to inform other controllers responsible for processing these personal data that a data subject has requested that they erase all links to these personal data, together with any copies or replications thereof (Art. 17 (2) GDPR).</li>
+                     <li>By way of exception, the right to erasure (“right to be forgotten”) does not apply if processing of the data in question is required for the reasons set forth in Art. 17 (3) GDPR. This may be the case, for example, insofar as processing is required to meet a legal obligation, or to establish, exercise or defend legal claims (Art. 17 (3) lit. a) and e) GDPR).</li>
+                     <li><strong>Right to demand processing restrictions</strong><br /> Under certain conditions, Art. 18 GDPR also guarantees your right to restriction of processing of these data. This means that you have the right to request that we restrict processing if one of the conditions listed in Art. 18 (1) GDPR is met. This may be the case, for example, if you dispute the accuracy of personal data about you. In this case, processing is restricted for a period sufficient to permit us to review the personal data for accuracy (Art. 18 (1) lit. a) GDPR). Restriction involves the marking of stored personal data with the goal of restricting their future processing (Art. 4 No. 3 GDPR). </li>
+                        <li><strong>Right to data portability</strong><br />As a data subject, you also have a right to data portability in accordance with Art. 20 GDPR. This means that you generally have the right to receive the personal data about you that you have provided to us, presented in a structured, commonly used and machine-readable format, and that you have the right to transmit these data to another data controller unimpeded by us, provided that the processing is based on consent issued pursuant to Art. 6 (1) lit. a) or Art. 9 (2) lit. a) GDPR, or on a contract pursuant to Art. 6 (1) lit. b) GDPR, and provided that such processing is carried out with the aid of automated procedures (Art. 20 (1) GDPR).
+In exercising your right to data portability, you always have the right to take steps to see to it that your personal data are transmitted directly by us to another data controller, provided that this is technically feasible (Art. 20 (2) GDPR).</li>
+<li><strong>Right to object</strong><br />
+Art. 21 GDPR guarantees your right to object. We expressly inform you, as a data subject, of your right to object not later than upon our first communication with you.
+
+Please find more detailed information below:</li>
+<li><strong>Right to object on grounds resulting from the situation specific to the data subject</strong><br />As a data subject, you have the right to object at any time, on grounds relating to your particular situation, to processing of personal data concerning you on the basis of Art. 6 (1) lit e) or f) GDPR, including profiling based on those provisions.
+
+ 
+
+In the case of an objection for reasons stemming from your particular situation, we will no longer process the personal data unless we can demonstrate compelling legitimate grounds for the processing and these grounds override your interests, rights and freedoms, or the processing is in service of establishment, exercise or defence of legal claims.</li>
+        <li><strong>The right to object to direct marketing</strong><br />If personal data is processed to engage in direct advertising, you have the right to at any time object to the processing of your affected personal data for the purposes of such advertising. This also applies to profiling to the extent that it is affiliated with such direct advertising.
+
+In the event that you object to processing for direct marketing purposes, we will no longer process the personal data concerned for these purposes</li>        
+            <li><strong>Right to revoke consent</strong><br />If the processing is based on consent pursuant to Art. 6 (1) lit. a) or Art. 9 (2) lit. a) GDPR, you as a data subject have the right to revoke your consent at any time pursuant to Art. 7 (3) GDPR. Revocation of consent will not affect the legality of processing undertaken on the basis of this consent prior to its revocation. We will bring this to your attention before you grant your consent.</li>
+                
+
+                </ul>
+            </section>
+
+            <section>
+                <h2>Right to lodge a complaint with a supervisory authority</h2>
+                <p>
+                As a data subject, you have the right to lodge a complaint with a supervisory authority subject to the conditions as set forth in Art. 77 GDPR.
+
+ 
+
+The supervisory authority responsible for us is the State Officer for Data Protection and Freedom of Information of the German state of Baden-Württemberg.
+
+You can find contact data here: <a href="https://www.baden-wuerttemberg.datenschutz.de/kontakt/" target="_blank" rel="noopener noreferrer" className='text-[#E87223]'>https://www.baden-wuerttemberg.datenschutz.de/kontakt/</a>.
+                </p>
+            </section>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
