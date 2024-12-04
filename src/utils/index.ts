8465b401e56cabf919d660b4df46b5533ed88ea0
Auspicious <info@auspicioussoft.com>
2024-12-04 18:31:51 +0530

    Add function to retrieve image URL from Flask proxy and import Axios instance

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import { getAxiosInstance } from "./axios"
 
 export const getDbImageUrl = (key: string) => {
     return `${process.env.NEXT_PUBLIC_BACKEND_URL}/uploads/${key}`
@@ -5,6 +6,15 @@ export const getDbImageUrl = (key: string) => {
 
 
 export const getImageUrlOfS3 = (subPath: string): string => {
-    const path =  `${process.env.NEXT_PUBLIC_AWS_BUCKET_PATH}${subPath}`
+    const path = `${process.env.NEXT_PUBLIC_AWS_BUCKET_PATH}${subPath}`
     return path
+}
+
+export const getImageUrlFromFlaskProxy = async (subPath: string) => {
+    try {
+        const axiosInstance = await getAxiosInstance()
+        return axiosInstance.get(`/file/${subPath}`)
+    } catch (error) {
+        console.log('error: ', error)
+    }
 }
