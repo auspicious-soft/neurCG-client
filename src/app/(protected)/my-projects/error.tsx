a8b88353df78f0cfb009f0dabd3476d80565d696
Auspicious <info@auspicioussoft.com>
2025-01-07 09:41:01 +0530

    Add error page components for protected routes in my-projects and my-uploads

---
+++ src/app/(protected)/my-projects/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const ErrorPage: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <p>Sorry, something went wrong on our end. Please try again later.</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
