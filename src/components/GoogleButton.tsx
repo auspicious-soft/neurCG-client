3044111daf0a551f6efe202e222766e1e36268e3
Auspicious <info@auspicioussoft.com>
2025-01-07 10:01:05 +0530

    Implement transition handling in GoogleButton for improved login experience

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useTransition } from 'react'
 import GoogleSvg from '@/assets/images/google.svg'
 import Image from 'next/image'
 import { handleGoogleLogin } from '@/actions'
 
 const GoogleButton = () => {
-
+    const [isPending, startTransition] = useTransition()
     return (
-        <div onClick={() => handleGoogleLogin()} className='flex items-center bg-[#4285F4] gap-4 mb-2 cursor-pointer'>
+        <div onClick={() => {
+            startTransition(async () => {
+                await handleGoogleLogin()
+            })
+        }} className={`flex items-center gap-4 mb-2 cursor-pointer bg-[#4285F4] ${isPending ? 'opacity-50' : 'opacity-100'}`}>
             <Image src={GoogleSvg} alt="google" width={40} height={43} className="h-[43px] object-contain pl-[4px]" />
             <p className='text-white font-medium'>Continue with Google</p>
         </div>
